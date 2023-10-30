80c83eefb9186646070a95ebee08d7bc8863eaad
Ruchi <ruchiagrawal9186@gmail.com>
2023-10-30 13:06:53 -0800

    todo app done

---
+++ todowithcontext/src/Contexts/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext,useContext } from "react";
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"todo message",
+            completed:false
+        },
+
+    ],
+    addTodo:(todo)=>{},
+    updateTodo:(todo,id)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+})
+
+export const useTodo = () =>
+{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
